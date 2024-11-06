95c3a4dd99031ea2d7cf1930fe1c663c0b4bd108
HubertTW <boney.chang@gmail.com>
2024-11-06 09:15:20 +0000

    multi sanitization done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bincode;
 use std::fs;
 use std::fs::File;
+use glob::glob;
 use std::io::{Cursor, Write};
 use std::ops::Deref;
 use std::time::{Duration, Instant};
@@ -10,19 +11,20 @@ use tfhe::prelude::{FheDecrypt, FheEncrypt, FheTrivialEncrypt};
 use tfhe::{set_server_key, ClientKey, FheUint, FheUint16, FheUint16Id, FheUint8, FheUint8Id, ServerKey, FheUint32Id, FheUint32};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut k = 4;
+    let mut k = 3;
     let mut modulo = 47u64;
-    let final_state:Vec<u16> = vec![1,2];
-    let string_size = 6;
-    let mut coef: Vec<u64> = vec![25, 0, 9, 24, 3, 24, 40, 30, 13, 35, 33, 38, 18, 44, 7, 37, 3, 33, 11, 46];
-    let chars = ['o','f','e'];
-    let code:Vec<u8> = vec![1,2,3,4];
+    let final_state:Vec<u16> = vec![1];
+    let string_size = 10;
+    let string_number = 3;
+    let mut coef: Vec<u64> = vec![4, 41, 45, 44, 34, 46, 32, 37];
+    let chars = ['f',' '];
+    let code:Vec<u8> = vec![1,2,3];
 
     let mut file = fs::read("server_key.bin")?;
     let sk = deserialize_sk(file.as_slice())?;
     set_server_key(sk);
 
-    println!("deserializing client key...");
+    println!("DEBUG: deserializing client key...");
     let mut byte_vec = fs::read("client_key.bin")?;
     let ck = deserialize_ck(&byte_vec.into_boxed_slice().deref())?;
 
@@ -38,9 +40,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut enc_str =  enc_ascii.clone();
 
+    let file = fs::read("encrypted_ascii.bin")?;
+    let mut enc_ascii = deserialize_str(&file, string_size)?;
+
+    let mut v_string = vec![];
+    let mut v_lastchar = vec![];
+
+    for entry in glob("string*").expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                println!("Reading file: {:?}", path.display());
+                let mut file = fs::read(&path)?;
+                v_string.push(deserialize_str(&file, string_size)?);
+            },
+            Err(e) => println!("Error reading file: {:?}", e),
+        }
+    }
+    for entry in glob("last*").expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                println!("Reading file: {:?}", path.display());
+                let mut file = fs::read(&path)?;
+                v_lastchar.push(deserialize_str(&file, string_size)?);
+            },
+            Err(e) => println!("Error reading file: {:?}", e),
+        }
+    }
     println!("encoding...");
-
-
+    let mut enc_zero = FheUint16::encrypt_trivial(0u8);
+    let mut enc_one = FheUint16::encrypt_trivial(1u8);
     let ascii_bytes: Vec<u8> = chars.iter().map(|&c| c as u8).collect();
     let mut enc_chars = vec![];
     let mut enc_code = vec![];
@@ -50,7 +78,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for i in code.clone(){
         enc_code.push(FheUint16::encrypt_trivial(i));
     }
-    let mut enc_zero = FheUint16::encrypt_trivial(0u8);
+
     for (i,ascii_val) in enc_str.clone().iter().enumerate(){
         let mut count = enc_zero.clone();
         for (j,chars_val) in enc_chars.clone().iter().enumerate() {
@@ -75,23 +103,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("poly degree: {}", len_coef);
     let mut enc_coef = vec![];
     for i in coef {
-        enc_coef.push(FheUint16::encrypt(i, &ck));
+        enc_coef.push(FheUint16::encrypt_trivial(i)); //FheUint16::encrypt(i, &ck)
     }
 
-    let mut enc_modulo = FheUint16::encrypt(modulo, &ck);
+    let mut enc_modulo = FheUint16::encrypt_trivial(modulo);//FheUint16::encrypt(modulo, &ck);
     let mut enc_final_state = vec![];
 
     for i in final_state.clone(){
-       enc_final_state.push(FheUint16::encrypt(i, &ck));
+       enc_final_state.push(FheUint16::encrypt_trivial(i)); //FheUint16::encrypt(i, &ck)
     }
 
 
     println!("calculating poly...");
     //let mut state_debug = vec![];
     //let mut curr_m_debug = vec![];
+    let mut v_states = vec![];
     let mut curr_state = FheUint16::encrypt_trivial(0u8);
 
-    let measurements = 3;
+    let measurements = 1;
     let mut elapsed_times: Vec<Duration> = Vec::new();
     for _ in 0..measurements {
 
@@ -118,13 +147,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
 
             //let start_mod = Instant::now();
-            println!("modulo...");
+            println!("final modulo...");
             curr_state = &sum % &enc_modulo;
             //let duration_mod = start_mod.elapsed();
             //println!("the mod duration is {:?}", duration_mod);
 
-
             //state_debug.push(curr_state.clone());
+            v_states.push(curr_state.clone());
         }
 
         let elapsed = start.elapsed();
@@ -138,53 +167,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Average poly elapsed time: {:?}", average_elapsed);
 
-    let debug_state:u8 = curr_state.decrypt(&ck);
-    println!("debug curr state {:?}", debug_state);
+    //let debug_state:u8 = curr_state.decrypt(&ck);
+    //println!("debug curr state {:?}", debug_state);
 
-    //TODO: multi final states
-    //let matching_res: FheUint16 = curr_state.ne(enc_final_state).cast_into();
-    let mut matching_count:FheUint16 = enc_zero.clone();
-    for i in enc_final_state.clone(){
-        matching_count = matching_count + FheUint16::cast_from(curr_state.eq(i));
-    }
-    let matching_res: FheUint16 = FheUint16::cast_from(matching_count.eq(enc_zero.clone()));
-    /* 1: not matching; 0: matching */
+        //TODO: new sanitization
+    for i in 0..string_number{
+        for (idx, val) in v_states.iter().enumerate() {
+            v_lastchar[i][idx] = &v_lastchar[i][idx] * val.clone();
+        }
+        let mut string_final_state = enc_zero.clone();
+            for val in v_lastchar[i].clone(){
+            string_final_state += val;
+            }
 
-    println!("sanitization...");
+        println!("checking accepting state...");
+        let mut matching_count:FheUint16 = enc_zero.clone();
+        for i in enc_final_state.clone(){
+            matching_count = matching_count + FheUint16::cast_from(string_final_state.eq(i));
+        }
+        let matching_res = matching_count.eq(enc_zero.clone());
+        //let matching_res: FheUint16 = FheUint16::cast_from(matching_count.eq(enc_zero.clone()));
+        /* 1: not matching; 0: matching */
 
-    let measurements = 10;
-    let mut elapsed_times: Vec<Duration> = Vec::new();
-
-    let mut sanitized_v = vec![];
-    for enc_x in enc_ascii.clone() {
-        sanitized_v.push(&matching_res * enc_x);
-    }
-
-    for _ in 0..measurements {
-
-        let start = Instant::now();
-        for enc_x in enc_ascii.clone() {
-            &matching_res * enc_x;
+        println!("sanitization...");
+        for idx in 0..string_size {
+            let idx_usize = idx as usize;
+           let position_check =  v_string[i][idx_usize.clone()].eq(&enc_zero);
+            v_string[i][idx_usize.clone()] = position_check.if_then_else(&matching_res.if_then_else(&enc_one, &enc_zero), &enc_one);
+            enc_ascii[idx_usize.clone()] = &v_string[i][idx_usize.clone()] * &enc_ascii[idx_usize.clone()];
         }
 
-        let elapsed = start.elapsed();
-        elapsed_times.push(elapsed);
-
-        println!("sanitization Elapsed time: {:?}", elapsed);
     }
-
-    // 計算平均經過時間
-    let total_elapsed: Duration = elapsed_times.iter().sum();
-    let average_elapsed = total_elapsed / (measurements as u32);
-
-    println!("Average sanitization elapsed time: {:?}", average_elapsed);
-
-
 
 
     println!("serialization...");
     let mut serialized_enc_str = Vec::new();
-    for i in &sanitized_v {
+    for i in &enc_ascii {
         bincode::serialize_into(&mut serialized_enc_str, &i)?;
     }
     let mut file_str = File::create("sanitized_payload.bin")?;
@@ -192,7 +210,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("done");
 
     println!("[debug] decrypt sanitized result");
-    let s = decryptStr(sanitized_v, &ck);
+    let s = decryptStr(enc_ascii, &ck);
     println!("the sanitized res is {:?}", s);
 
     /*
